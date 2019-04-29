2bb260b17e16f47b3f0f887713a343268e6ecef6
Phi Hai Nguyen <phihai93@gmail.com>
2019-04-29 10:21:17 -0700

    Updates

--- precache-manifest.63140887bb00e4ab6d407f854ca34820.js
+++ precache-manifest.63140887bb00e4ab6d407f854ca34820.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8203f4bca92fe0bfc7356243368e404c",
+    "revision": "717384a5d73dad93ef05ace33fcd5186",
     "url": "/clickity-game/index.html"
   },
   {
-    "revision": "d904bae1d17b126de0bf",
-    "url": "/clickity-game/static/css/main.98a4e50a.chunk.css"
+    "revision": "c5ad0a26799d1f2d9469",
+    "url": "/clickity-game/static/css/main.48ca40ca.chunk.css"
   },
   {
     "revision": "4743c039c04edea7e174",
     "url": "/clickity-game/static/js/2.1db7aab6.chunk.js"
   },
   {
-    "revision": "d904bae1d17b126de0bf",
+    "revision": "c5ad0a26799d1f2d9469",
     "url": "/clickity-game/static/js/main.03a78140.chunk.js"
   },
   {
